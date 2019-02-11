b16236f93725501cec6b44c8d88092d74194ff16
Valorie <valoriejones@gmail.com>
2019-02-10 18:29:30 -0600

    random quiz solution

---
+++ projects/dodgev/quiz/testme.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    char *chars = "aerstx[({ })]";
+    int c = rand() % 13;
+    return chars[c];
+}
+
+char *inputString()
+{
+    int strsize = 6, i;
+    char *str;
+    str = (char *) malloc(strsize);
+    if(str==0) exit(99);
+    memset(str ,0 , strsize);
+    for(i=0; i<strsize-1;i++){
+      str[i] = inputChar();
+    }
+    return str;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      free(s);
+      printf("error ");
+      exit(200);
+    }
+    free(s);
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
